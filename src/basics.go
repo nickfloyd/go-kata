226e47edff7de97ec6d029ae9dd64013f540224a
Nick Floyd <nicholas.floyd.info@gmail.com>
2020-08-21 20:15:53 +0000

    adds conditionals scenarios

--- src/basics.go
+++ src/basics.go
@@ -20,3 +20,16 @@ func assignments(x int, y int) int {
 	fmt.Printf("Mean: %v.  Type: %T\n", mean, mean)
 	return mean
 }
+
+func conditionals(val int) string {
+	// Compaire the value passed using if/else and return the following messages
+	// if val is less than 5 then return "The value is SMALL"
+	// if the value is between 6 and 10 the return "The value is MEDIUM"
+	// if the value is greater than 10 then return "The value is LARGE"
+
+	message := ""
+
+	// Solve
+
+	return message
+}
--- src/basics_test.go
+++ src/basics_test.go
@@ -11,3 +11,21 @@ func TestAssignments(t *testing.T) {
 		t.Errorf("Mean was incorrect, got: %d, want: %d.", result, 7)
 	}
 }
+
+type conditionalTestData struct {
+	n        int
+	expected string
+}
+
+var conditionalTests = []conditionalTestData {
+	{1, "The value is SMALL"}, {7, "The value is MEDIUM"}, {100, "The value is LARGE"},
+}
+
+func TestConditionals(t *testing.T) {
+	for _, tt := range conditionalTests {
+			actual := conditionals(tt.n)
+			if actual != tt.expected {
+					t.Errorf("conditionals(%d): expected: %s, actual: %s", tt.n, tt.expected, actual)
+			}
+	}
+}
